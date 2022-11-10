d4d68f53f6600b7633dde429c39b23cbde917c48
josephaorahim <josephaorahim@yahoo.com>
2022-11-10 15:47:59 -0700

    break

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+@app.get("/users/me")
+async def read_user_me():
+    return { "user_id": "the current user" }
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return { "user_id": user_id }
